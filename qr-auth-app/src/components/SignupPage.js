8dbe2e07489e9f6ae844c6c4d853d7acd2f89e25
ghwi <ss77991@naver.com>
2025-05-22 19:28:42 +0900

    init com24

--- qr-auth-app/src/components/SignupPage.js
+++ qr-auth-app/src/components/SignupPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import API from './api';
+import API from '../api';
 
 function SignUpPage() {
   const [form, setForm] = useState({ username: '', password: '', name: '', age: '' });
