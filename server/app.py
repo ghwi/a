e116cdf61c2023e9158b916585792893671c557f
ghwi <ss77991@naver.com>
2025-05-22 18:58:09 +0900

    init com23

--- server/app.py
+++ server/app.py
@@ -142,4 +142,5 @@ def verify_pr_code(current_user):
 
 # 서버 실행
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
