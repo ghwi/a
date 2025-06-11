987c7ac980a1552a84c73dbf900629222f83dcca
ghwi <ss77991@naver.com>
2025-06-12 03:32:12 +0900

    init com53

--- server/app.py
+++ server/app.py
@@ -123,5 +123,3 @@ def home():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    port = int(os.getenv("PORT") or 5000)
-    socketio.run(app, host="0.0.0.0", port=port)
